f1054e316e23bfdcefb8e91a24548325cd469ecb
marianna-meli <marianna.rodrigues@mercadolivre.com>
2022-03-23 18:47:47 -0300

    Creating alphabetical order list.

--- src/main/java/com/mercadolivre/grupo4/desafiospring/controller/ProductController.java
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.mercadolivre.grupo4.desafiospring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,12 +20,6 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/api/v1/products")
-    public ResponseEntity<List<ProductDTO>> listAllProducts() {
-        List<ProductDTO> convertedList = ProductDTO.convert(productService.listAllProducts());
-        return ResponseEntity.status(HttpStatus.OK).body(convertedList);
-
-    }
 
     @PostMapping("/api/v1/insert-articles-request")
     public ResponseEntity<List<ProductDTO>> insertProduct(@RequestBody List<Product> listProduct) {
@@ -39,5 +30,47 @@ public class ProductController {
         return null;
 
 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/api/v1/products")
+    public ResponseEntity<List<ProductDTO>> listAllProducts() {
+        List<ProductDTO> convertedList = ProductDTO.convert(productService.listAllProducts());
+        return ResponseEntity.status(HttpStatus.OK).body(convertedList);
+
+    }
+
+    @GetMapping("/api/v1/products")
+    public ResponseEntity<List<ProductDTO>> listAlphabeticalOrder(@RequestParam String order) {
+        List<ProductDTO> convertedList = ProductDTO.convert(productService.listAlphabeticalOrder())
     }
 }
--- src/main/java/com/mercadolivre/grupo4/desafiospring/service/ProductService.java
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/service/ProductService.java
@@ -5,6 +5,7 @@ import com.mercadolivre.grupo4.desafiospring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -13,9 +14,58 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public List<Product> listAllProducts() {
         return productRepository.readAllProducts();
     }
 
 
+    public List<Product> listAlphabeticalOrder() {
+        List<Product> rawList = productRepository.readAllProducts();
+        return rawList.sort(Comparator.comparing(Product::getName));
+
+    }
 }
