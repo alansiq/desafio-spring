c5ed6d45643422bb4616be7f4bae0ee67d8e51d8
Rafael Santana <rafael.santana@mercadolivre.com>
2022-03-23 15:58:27 -0300

    Created entity Product

---
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/entity/Product.java
@@ -0,0 +1,22 @@
+package com.mercadolivre.grupo4.desafiospring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    
+    private Long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private BigDecimal price;
+    private Integer quantity;
+    private Boolean freeShipping;
+    private String prestige;
+}
