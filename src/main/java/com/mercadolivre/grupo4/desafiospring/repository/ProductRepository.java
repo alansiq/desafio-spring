1344c65019f01d00fbb2ef1b1a58a9ab8f33d8f7
Rafael Santana <rafael.santana@mercadolivre.com>
2022-03-23 16:17:49 -0300

    Created Product Repository with method to read JSON File

---
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/repository/ProductRepository.java
@@ -0,0 +1,34 @@
+package com.mercadolivre.grupo4.desafiospring.repository;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolivre.grupo4.desafiospring.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+    private static final String JSON_FILE = "products.json";
+
+    private List<Product> jsonParaList(String arquivoJSON){
+        List<Product> result = new ArrayList<>();
+        try{
+            byte[] mapData = Files.readAllBytes(Paths.get(arquivoJSON));
+            Product[] personagens = null;
+            ObjectMapper objectMapper = new ObjectMapper();
+            personagens = objectMapper.readValue(mapData, Product[].class);
+            result = Arrays.asList(personagens);
+        } catch (JsonMappingException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
