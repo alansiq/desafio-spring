8c05bfa1731e3cb802b1e93fb2d86e03df51f9e8
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-24 15:40:03 -0300

    Added basic compound filtering

--- src/main/java/com/mercadolivre/grupo4/desafiospring/service/ProductService.java
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/service/ProductService.java
@@ -6,9 +6,13 @@ import com.mercadolivre.grupo4.desafiospring.entity.Product;
 import com.mercadolivre.grupo4.desafiospring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
 public class ProductService {
@@ -27,5 +31,48 @@ public class ProductService {
     public boolean save(List<Product> productList) {
         return productRepository.addList(productList);
 
+    }
+
+    public List<ProductDTO> listFilteredProducts(Optional<String> name,
+                                                 Optional<String> category,
+                                                 Optional<String> brand,
+                                                 Optional<BigDecimal> price,
+                                                 Optional<Boolean> freeShipping,
+                                                 Optional<String> prestige)
+    {
+
+        List<Product> resultList = productRepository.get();
+
+        if (name.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getName().equals(name.get())).collect(Collectors.toList());
+        }
+
+        if (category.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getCategory().equals(category.get())).collect(Collectors.toList());
+        }
+
+        if (brand.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getBrand().equals(brand.get())).collect(Collectors.toList());
+        }
+
+        if (price.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getPrice().equals(price.get())).collect(Collectors.toList());
+        }
+
+        if (freeShipping.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getFreeShipping().equals(freeShipping.get())).collect(Collectors.toList());
+        }
+
+        if (prestige.isPresent()) {
+            resultList =
+                    resultList.stream().filter(product -> product.getPrestige().equals(prestige.get())).collect(Collectors.toList());
+        }
+
+        return ProductDTO.convert(resultList);
     }
 }
