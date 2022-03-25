7c5d21942dd5c935d79cf4370137c9dd6b032fff
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-25 15:07:15 -0300

    fixed imports

--- src/main/java/com/mercadolivre/grupo4/desafiospring/controller/ProductController.java
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/controller/ProductController.java
@@ -12,10 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.List;
