68cb06822a73997f9c64b58c50234cfa606e6819
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-26 15:41:17 -0300

    fixed combined filtering

--- src/main/java/com/mercadolivre/grupo4/desafiospring/repository/ProductRepository.java
+++ src/main/java/com/mercadolivre/grupo4/desafiospring/repository/ProductRepository.java
@@ -43,59 +43,7 @@ public class ProductRepository implements IProductRepository {
         return result;
     }
 
-    public List<Product> orderByName(Integer order){
-        List<Product> ordered = this.getAll().stream()
-                .sorted(Comparator.comparing(Product::getName)).collect(Collectors.toList());
-        if (order == 1) {
-            Comparator<Product> comparator = Comparator.comparing(Product::getName);
-            ordered.sort(comparator.reversed());
-            return ordered;
-        } else {
-            ordered = orderByPrice(order);
-        }
-        return ordered;
-    }
 
-    public List<Product> orderByPrice(Integer order){
-        List<Product> ordered = this.getAll().stream()
-                .sorted(Comparator.comparing(Product::getPrice)).collect(Collectors.toList());
-        if (order == 3) {
-            Comparator<Product> comparator = Comparator.comparing(Product::getPrice);
-            ordered.sort(comparator.reversed());
-            return ordered;
-        }
-        return ordered;
-    }
-
-    public List<Product> filterByName(String name) {
-        return this.getAll().stream()
-                .filter(product -> product.getName().equals(name)).collect(Collectors.toList());
-    }
-
-    public List<Product> filterByPrice(BigDecimal price) {
-        return this.getAll().stream()
-                .filter(product -> product.getPrice().equals(price)).collect(Collectors.toList());
-    }
-
-    public List<Product> filterByShipping(Boolean freeShipping) {
-        return this.getAll().stream()
-                .filter(product -> product.getFreeShipping().equals(freeShipping)).collect(Collectors.toList());
-    }
-
-    public List<Product> filterByCategory(String category) {
-        return this.getAll().stream()
-                .filter(product -> product.getCategory().equals(category)).collect(Collectors.toList());
-    }
-
-    public List<Product> filterByBrand(String brand) {
-        return this.getAll().stream()
-                .filter(product -> product.getBrand().equals(brand)).collect(Collectors.toList());
-    }
-
-    public List<Product> filterByPrestige(String prestige) {
-        return this.getAll().stream()
-                .filter(product -> product.getPrestige().equals(prestige)).collect(Collectors.toList());
-    }
 
     @Override
     public boolean addList(List<Product> productsToAddList) {
